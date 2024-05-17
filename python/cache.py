64820c51ffcf02b4a315e707c712075c85e6fdca
Erik Adelbert <erik@adelbert.fr>
2024-05-17 16:40:06 +0200

    Remove unused async cache decorator

--- python/cache.py
+++ python/cache.py
@@ -7,21 +7,10 @@ from asyncio import ensure_future
 from functools import lru_cache
 
 
-def async_lru_cache_decorator(async_function):
-    """parameterless decorator"""
-
-    @lru_cache
-    def cached_async_function(*args, **kwargs):
-        coroutine = async_function(*args, **kwargs)
-        return ensure_future(coroutine)
-
-    return cached_async_function
-
-
 def async_lru_cache(*args, **kwargs):
     """decorator with options"""
 
-    def async_lru_cache_decorator(  # pylint: disable="redefined-outer-name"
+    def async_lru_cache_decorator(
         async_function,
     ):
         @lru_cache(*args, **kwargs)
