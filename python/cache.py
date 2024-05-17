21581db53d9807adf951330b5bb42dd182117418
Erik Adelbert <erik@adelbert.fr>
2024-05-17 17:10:51 +0200

    Tighten and test async lru cache

--- python/cache.py
+++ python/cache.py
@@ -7,15 +7,16 @@ from asyncio import ensure_future
 from functools import lru_cache
 
 
-def async_lru_cache(*args, **kwargs):
+def async_lru_cache(*lru_cache_args, **lru_cache_kwargs):
     """decorator with options"""
 
     def async_lru_cache_decorator(
         async_function,
     ):
-        @lru_cache(*args, **kwargs)
-        def cached_async_function(*args, **kwargs):
-            coroutine = async_function(*args, **kwargs)
+
+        @lru_cache(*lru_cache_args, **lru_cache_kwargs)
+        def cached_async_function(*fun_args, **fun_kwargs):
+            coroutine = async_function(*fun_args, **fun_kwargs)
             return ensure_future(coroutine)
 
         return cached_async_function
---
+++ python/test_cache.py
@@ -0,0 +1,30 @@
+"""
+test cache/cache.py
+"""
+
+from pytest import mark
+from cache import async_lru_cache
+
+
+@mark.asyncio
+async def test_async_cache_decorator_should_cache_async_func():
+    """
+    Interposes cache when called
+    """
+    pid = _positive()
+
+    @async_lru_cache(maxsize=2)
+    async def to_be_decorated(key: int) -> int:
+        _ = key
+        return next(pid)
+
+    assert await to_be_decorated(0xDEADBEEF) == 1
+    assert await to_be_decorated(0x8BADF00D) == 2
+    assert await to_be_decorated(0xDEADBEEF) == 1  # next is not called again
+
+
+def _positive():
+    i = 1
+    while True:
+        yield i
+        i += 1
